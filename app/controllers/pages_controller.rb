8eb03e60cb06e8f4728b3c776adf27d92340aab3
Mar Gabriel <gabytabilla@gmail.com>
2017-04-14 03:01:34 +0000

    Finishing Task 3

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,8 +4,8 @@ class PagesController < ApplicationController
   end
 
   def sms
-    # @number = params.permit[:number]
-    # @message = params.permit[:message]
+    @number = params[:number]
+    @message = params[:message]
     # byebug
     # raise "Action sms in the UsersController is not implemented! received params #{params.to_json}"
   end
